8c2842e6b4cffcd2a4758ead447003dfca4deb54
Vishal Kumar <vishalkumar51095@gmail.com>
2025-06-06 15:07:48 +0530

    Circuit Breaker Implemented

--- UserService/src/main/java/com/vishalkumar/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/vishalkumar/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.vishalkumar.user.service.controllers;
 
 import com.vishalkumar.user.service.entities.User;
 import com.vishalkumar.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,29 +16,43 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
-    //create
+    // Create a user
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
-        User user1=userService.saveUser(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
-    //single user get
+    // Get a single user with circuit breaker
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getSingleUser(@PathVariable String userId){
-        User user= userService.getUser(userId);
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        logger.info("Get single user handler: UserController");
+        User user = userService.getUser(userId);  // This must make REST calls to external services
         return ResponseEntity.ok(user);
     }
 
-    // all user get
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUser(){
-        List<User> allUser = userService.getAllUser();
-        return ResponseEntity.ok(allUser);
+    // Fallback method for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Throwable ex) {
+        logger.warn("Fallback executed for userId: {}, because: {}", userId, ex.toString());
+        User user = User.builder()
+                .userId("141234")
+                .name("Dummy Vishal")
+                .email("vishal@fallback.com")
+                .about("This user is returned as fallback because some service is down")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-
+    // Get all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> allUsers = userService.getAllUser();
+        return ResponseEntity.ok(allUsers);
+    }
 }
